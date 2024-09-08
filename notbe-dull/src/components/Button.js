d26feba6500cc67c0b9e27c110dae4d468a36ba1
Liyanage C.V.K <130178817+Vimodya@users.noreply.github.com>
2024-09-08 10:33:53 +0100

    style: add styles
    add some styles to app and button

--- notbe-dull/src/components/Button.js
+++ notbe-dull/src/components/Button.js
@@ -2,8 +2,7 @@ import React from "react";
 import "./../Button.css";
 
 const Button = (props) => {
-  return <button onClick={props.callApi}>Click to generate a joke.</button>;
+  return <button onClick={props.callApi}>Tell me a Joke</button>;
 };
 
-// Export Button Component
 export default Button;
