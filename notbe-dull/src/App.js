6eb1e7965f1f81e2c20919ee021049e5e546139d
Liyanage C.V.K <130178817+Vimodya@users.noreply.github.com>
2024-09-08 10:24:06 +0100

    feat: change the visiulity
    make the background and add box element to appear great

--- notbe-dull/src/App.js
+++ notbe-dull/src/App.js
@@ -1,10 +1,18 @@
 import Joke from "./components/Jokes";
+import "./App.css";
+import background from "./image.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Joke Generator Using React and Joke API</h1>
-      <Joke />
+    <div
+      className="App-header"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="box">
+        <h1 className="h1">Are you Dull ? </h1>
+        <h2>Need to refresh 😍 </h2>
+        <Joke />
+      </div>
     </div>
   );
 }
